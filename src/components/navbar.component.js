c1caf703fa4836a0e25250fcb34326b31c4220d8
sachithanjula <41498657+sachithanjula@users.noreply.github.com>
2020-06-29 04:34:44 +0530

    initial commit

---
+++ src/components/navbar.component.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import {Link} from 'react-router-dom'
+
+// const Navbar = () => {
+//     return(
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <Link to="/" className="navbar-brand">MERN-STACK-TODO-APP</Link>
+//             <div className="collapse navbar-collapse">
+//                 <ul className="navbar-nav mr-auto">
+//                     <li className="navbar-item">
+//                     <Link to="/" className="nav-link">TODOS</Link>
+//                     </li>
+//                     <li className="navbar-item">
+//                     <Link to="/create" className="nav-link">CREATE TODO</Link>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default (Navbar);
